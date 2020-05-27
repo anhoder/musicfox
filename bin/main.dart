9ab03b8d4d64868530464fd4361749ad08230f9c
aihailin <1766447919@qq.com>
2020-05-27 13:40:19 +0800

    update. progress

--- bin/main.dart
+++ bin/main.dart
@@ -3,12 +3,17 @@ import 'dart:convert';
 import 'dart:io';
 
 import 'package:console/console.dart';
+import 'package:musicfox/rainbow_progress.dart';
 import 'package:musicfox/window_ui.dart';
 
 void main(List<String> arguments) {
   // var ui = WindowUI();
   // ui.display();
-  var progress = WideLoadingBar();
-  var timer = progress.loop();
+  var progress = RainbowProgress();
+  var i = 0;
+  Timer.periodic(Duration(milliseconds: 50), (timer) {
+    progress.update(i++);
+    if (i > progress.complete) timer.cancel();
+  });
   
 }
--- lib/rainbow_progress.dart
+++ lib/rainbow_progress.dart
@@ -1,5 +1,73 @@
 import 'package:console/console.dart';
 
-class RainbowProgress extends ProgressBar {
+class RainbowProgress {
+  final int complete;
+  int current = 0;
+  int width;
+  bool showPercent;
+  String completeChar;
+  String forwardChar;
+  String unfinishChar;
+  String leftDelimiter;
+  String rightDelimiter;
+  bool rainbow;
 
+
+  /// Creates a Progress Bar.
+  ///
+  /// [complete] is the number that is considered 100%.
+  RainbowProgress(
+    {this.complete = 100, 
+    this.width, 
+    this.completeChar = '=',
+    this.forwardChar = '>',
+    this.unfinishChar = ' ',
+    this.leftDelimiter = '[',
+    this.rightDelimiter = ']',
+    this.rainbow = true,
+    this.showPercent = true}) {
+    width ??= Console.columns;
+  }
+
+  void update(int progress) {
+    if (progress == current) {
+      return;
+    }
+
+    current = progress;
+
+    var ratio = progress / complete;
+    var percent = (ratio * 100).toInt();
+
+    var digits = percent.toString().length;
+
+    var w = showPercent ? width - digits - 4 : width - 4;
+
+    var count = (ratio * w).toInt();
+    var before = showPercent ? '${percent}% ${leftDelimiter}' : leftDelimiter;
+    var after = rightDelimiter;
+
+    var out = StringBuffer(before);
+
+    for (var x = 1; x < count; x++) {
+      out.write(completeChar);
+    }
+
+    out.write(forwardChar);
+
+    for (var x = count; x < w; x++) {
+      out.write(unfinishChar);
+    }
+
+    out.write(after);
+
+    if (out.length - 1 == Console.columns) {
+      var it = out.toString();
+
+      out.clear();
+      out.write(it.substring(0, it.length - 2) + rightDelimiter);
+    }
+
+    Console.overwriteLine(out.toString());
+  }
 }
