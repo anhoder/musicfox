df95c721d67343ef12c1cc9311e0dde81cd9cbd4
AlanAlbert <alan1766447919@gmail.com>
2020-08-02 20:13:40 +0800

    update. win

--- lib/ui/main_ui.dart
+++ lib/ui/main_ui.dart
@@ -143,7 +143,7 @@ class MainUI {
         _playerStatus.setStatus(status);
         displayPlayerUI();
         if (!Platform.isWindows && _playerStatus.status == Status.STOPPED) {
-          Timer(Duration(milliseconds: 1000), () async {
+          Timer(Duration(milliseconds: 1500), () async {
             if (_playerStatus.status == Status.STOPPED) {
               _watch.stop();
               _watch.reset();
